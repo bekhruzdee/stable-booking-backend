0e596140733630818e3be9a9f341a85b6bbddad0
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-04-16 18:16:09 +0500

    updated

--- src/users/dto/create-admin.dto.ts
+++ src/users/dto/create-admin.dto.ts
@@ -1,29 +1,28 @@
 import {
-    IsNotEmpty,
-    IsString,
-    IsEmail,
-    MinLength,
-    IsIn,
-  } from 'class-validator';
-  
-  export class CreateAdminDto {
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(3)
-    username: string;
-  
-    @IsNotEmpty()
-    @IsEmail()
-    email: string;
-  
-    @IsNotEmpty()
-    @IsString()
-    @MinLength(6)
-    password: string;
-  
-    @IsNotEmpty()
-    @IsString()
-    @IsIn(['admin'])
-    role: string;
-  }
-  
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  MinLength,
+  IsIn,
+} from 'class-validator';
+
+export class CreateAdminDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(3)
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsIn(['admin'])
+  role: string;
+}
