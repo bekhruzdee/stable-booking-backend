71b98a73b0e571e58c5fa341999dd275c1875ae4
bekhruzdee <qutbidinovbehruz003@gmail.com>
2025-04-17 13:41:24 +0500

    update create-horse.dto.ts

--- src/horse/dto/create-horse.dto.ts
+++ src/horse/dto/create-horse.dto.ts
@@ -1,7 +1,20 @@
-// create-horse.dto.ts
+import { IsInt, IsNotEmpty, IsPositive, IsString, Min } from 'class-validator';
+
 export class CreateHorseDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsInt()
+  @Min(0)
   age: number;
+
+  @IsNotEmpty()
+  @IsString()
   breed: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @IsPositive()
   ownerId: number;
 }
